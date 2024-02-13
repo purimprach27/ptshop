b592e8916d93a2008f6bf97d6d51aa5761d91364
purimprach27 <137262308+purimprach27@users.noreply.github.com>
2024-02-13 22:09:23 +0700

    test
    ทดสอบระบบ

---
+++ test1000.spec.ts
@@ -0,0 +1,22 @@
+import { test, expect } from '@playwright/test';
+
+test('test', async ({ page }) => {
+  await page.goto('http://localhost:8000/');
+  await page.getByRole('link', { name: 'voucher 1000 baht' }).first().click();
+  await page.getByRole('button', { name: ' Add to cart' }).click();
+  await page.getByRole('link', { name: ' Proceed to checkout' }).click();
+  await page.getByRole('link', { name: 'Proceed to checkout' }).click();
+  await page.getByLabel('Pay by bank wireSCB').check();
+  await page.getByText('Upload Slip').click();
+  await page.getByLabel('Upload Slip').setInputFiles('images.jpg');
+  await page.getByLabel('Yes').check();
+  await page.getByLabel('Name').click();
+  await page.getByLabel('Name').fill('firstname');
+  await page.getByLabel('Address').click();
+  await page.getByLabel('Address').fill('322');
+  await page.getByLabel('Tel').click();
+  await page.getByLabel('Tel').fill('020000');
+  await page.getByLabel('Tax ID').click();
+  await page.getByLabel('Tax ID').fill('123456');
+  await page.getByRole('button', { name: 'Place Order' }).click();
+});
---
+++ test500NoTax.spec.ts
@@ -0,0 +1,15 @@
+import { test, expect } from '@playwright/test';
+
+test('test', async ({ page }) => {
+  await page.goto('http://localhost:8000/');
+  await page.getByRole('link', { name: 'Voucher', exact: true }).click();
+  await page.getByRole('link', { name: 'voucher 500 baht' }).first().click();
+  await page.getByRole('button', { name: ' Add to cart' }).click();
+  await page.getByRole('link', { name: ' Proceed to checkout' }).click();
+  await page.getByRole('link', { name: 'Proceed to checkout' }).click();
+  await page.getByLabel('Pay by bank wireSCB').check();
+  await page.getByLabel('Upload Slip').click();
+  await page.getByLabel('Upload Slip').setInputFiles('Black and White Elegant Digital Marketing Resume.png');
+  await page.locator('#taxInvoiceOption').nth(1).check();
+  await page.getByRole('button', { name: 'Place Order' }).click();
+});
